36bc4d3135b618bf8c38fe9567f08817036645ee
Rafael Fernandes <rafael.fernandes.aju@gmail.com>
2022-11-17 10:36:08 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
-  return(
+  return (
     <>
-    <h1>Olá Mundo!</h1>    
+      <h1>Olá Mundo!</h1>
+      <NotificationButton />
     </>
   )
 }
