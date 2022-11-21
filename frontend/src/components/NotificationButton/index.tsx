423dc4de8b4f552cfe8f47a43b0a3aa9baeec40c
Rafael Fernandes <rafael.fernandes.aju@gmail.com>
2022-11-20 21:00:30 -0300

    Tost Info

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -2,6 +2,7 @@ import icon from '../../assets/img/notification-icon.svg';
 import './styles.css';
 import axios from 'axios';
 import { BASE_URL } from '../../utils/request';
+import { toast } from 'react-toastify';
 
 type Props = {
     saleId: number;
@@ -9,6 +10,9 @@ type Props = {
 
 function handleClick(id : number){
     axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(responde => {
+        toast.info("SMS enviado com sucesso!")
+    })
 }
 
 function NotificationButton( {saleId} : Props) {
